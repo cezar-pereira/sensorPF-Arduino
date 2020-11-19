42a49e5fb0eaa6e53c57e2c33371f8823b828a26
cezar.pereira <helaniocezar@gmail.com>
2020-11-19 14:08:28 -0300

    criada funcao para se conectar ao wifi, adicao do arquivo Credentials.h

--- projetoPF.ino
+++ projetoPF.ino
@@ -1,5 +1,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+
+#include "Credentials.h"
 
 OneWire oneWire(2);
 DallasTemperature sensor(&oneWire);
@@ -7,12 +10,32 @@ DallasTemperature sensor(&oneWire);
 void setup()
 {
     Serial.begin(115200);
+
+    wifiConnect();
+
     sensor.begin();
 }
 
 void loop()
 {
-    Serial.println(readTemperature());
+    if (!WiFi.isConnected())
+    {
+        Serial.println("Desconectado, reconectando...");
+        WiFi.reconnect();
+    }
+}
+
+void wifiConnect()
+{
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("\nconectando");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print("\nconectado: ");
+    Serial.println(WiFi.localIP());
 }
 
 float readTemperature()
