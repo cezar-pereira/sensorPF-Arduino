866d88c7f01dd32459a8fac537e41b707bc6b23c
cezar.pereira <helaniocezar@gmail.com>
2020-11-19 13:20:18 -0300

    criada funcao de leitura da temperatura

--- projetoPF.ino
+++ projetoPF.ino
@@ -1,7 +1,22 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire oneWire(2);
+DallasTemperature sensor(&oneWire);
+
 void setup()
 {
+    Serial.begin(115200);
+    sensor.begin();
 }
 
 void loop()
 {
+    Serial.println(readTemperature());
+}
+
+float readTemperature()
+{
+    sensor.requestTemperatures();
+    return sensor.getTempCByIndex(0);
 }
